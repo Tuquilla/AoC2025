06e485978a98891e72613c58187f0db24ae6e2bf
Kilian Glaus <kilian.glaus@hotmail.com>
2025-12-07 14:56:31 +0100

    Finish day7 part2

--- day7/main.go
+++ day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"AoC2025"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +16,7 @@ func main() {
 		grid = append(grid, lineArr)
 	}
 
+	// part 1
 	splits := 0
 	for row, _ := range grid {
 		for col, colElement := range grid[row] {
@@ -37,6 +39,56 @@ func main() {
 			}
 		}
 	}
-
 	fmt.Println("Total splits:", splits)
+
+	// part 2
+	inputAsSlice = AoC2025.ReadFile("./day7/input.txt")
+
+	for _, line := range inputAsSlice {
+		lineArr := strings.Split(line, "")
+		grid = append(grid, lineArr)
+	}
+
+	for row, _ := range grid {
+		for col, colElement := range grid[row] {
+			// skip first row
+			if row == 0 {
+				continue
+			}
+			// draw timelinenumber
+			if colElement == "." {
+				if grid[row-1][col] == "S" {
+					grid[row][col] = "1"
+				} else if grid[row-1][col] != "^" && grid[row-1][col] != "." {
+					grid[row][col] = grid[row-1][col]
+				}
+			}
+
+		}
+		for col2, colElement2 := range grid[row] {
+			if colElement2 == "^" && grid[row-1][col2] != "." {
+				numAbove, _ := strconv.Atoi(grid[row-1][col2])
+				if col2-1 >= 0 {
+					numberLeft, _ := strconv.Atoi(grid[row][col2-1])
+					grid[row][col2-1] = strconv.Itoa(numberLeft + numAbove)
+				}
+				if col2+1 < len(grid[row]) {
+					number, err := strconv.Atoi(grid[row][col2+1])
+					if err != nil {
+						grid[row][col2+1] = grid[row-1][col2]
+					} else {
+						grid[row][col2+1] = strconv.Itoa(number + numAbove)
+					}
+				}
+			}
+		}
+	}
+
+	timelines := 0
+	for _, element := range grid[len(grid)-1] {
+		number, _ := strconv.Atoi(element)
+		timelines += number
+	}
+
+	fmt.Println("Total timelines:", timelines)
 }
